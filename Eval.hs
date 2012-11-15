657e1bb83320eb24cea2bf8e7525c25528f27e27
Benjamin Malley <ben@undine>
2012-11-15 11:26:40 -0500

    added apply, numOp

--- Eval.hs
+++ Eval.hs
@@ -12,7 +12,8 @@ eval val@(Bool _) = val
 eval (List [Atom "quote", val]) = val
 eval (List (Atom f:args)) = apply f $ map eval args
 
-apply :: String
+apply :: String -> [LispVal] -> LispVal
+apply f args = maybe (Bool False) ($ args) $ Map.lookup f primitives
 
 primitives :: Map.Map String ([LispVal] -> LispVal)
 primitives = Map.fromList [("+", numOp (+)),
@@ -24,5 +25,11 @@ primitives = Map.fromList [("+", numOp (+)),
 	("remainder", numOp rem)]
 
 numOp :: (Num a) => (a -> a -> a) -> [LispVal] -> LispVal
-numOp op params = Number $ foldl1 op $ map unpackNum params
+numOp op params = Number $ foldl1 op $ map unpack params where
+	unpack :: LispVal -> Integer
+	unpack (Number n) = n
+	unpack (String s) = let parsed = reads n in
+		if null parsed then 0 else fst $ parsed !! 0
+	unpack (List [n]) = unpack n
+	unpack _ = 0
 
