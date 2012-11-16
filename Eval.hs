46f0b74e08d078dc493ea811ad9d0e2bca316253
Benjamin Malley <benjamin.malley@gmail.com>
2012-11-16 11:01:40 -0500

    fixed bugs preventing compilation

--- Eval.hs
+++ Eval.hs
@@ -13,9 +13,9 @@ eval val@(Number _) = return val
 eval val@(Bool _) = return val
 eval (List [Atom "quote", val]) = return val
 eval (List (Atom f:args)) = mapM eval args >>= apply f
-eval bad = throwError $ BadSpecialForm bad
+eval bad = throwError $ BadSpecialForm "bad" bad
 
-apply :: String -> [LispVal] -> LispVal
+apply :: String -> [LispVal] -> ThrowsError LispVal
 apply f args = maybe (throwError $ NotFunction "Unrecognized" f) ($ args) (Map.lookup f primitives)
 
 primitives :: Map.Map String ([LispVal] -> ThrowsError LispVal)
@@ -27,13 +27,14 @@ primitives = Map.fromList [("+", numOp (+)),
 	("quotient", numOp quot),
 	("remainder", numOp rem)]
 
+numOp :: (Integer -> Integer -> Integer) -> [LispVal] -> ThrowsError LispVal
 numOp op val@[_] = throwError $ NumArgs 2 val
-numOp op params = mapM unpack params >>= return Number . foldl1' op
+numOp op params = mapM unpack params >>= return . Number . foldl1' op
 
 unpack :: LispVal -> ThrowsError Integer
 unpack (Number n) = return n
 unpack (String s) = let parsed = reads s in
-	if null parsed then throwError $ TypeMismatch "number" $ String s else return $ fst $ parsed !! 0
+	if (null parsed) then (throwError $ TypeMismatch "number" $ String s) else (return $ fst $ parsed !! 0)
 unpack (List [n]) = unpack n
 unpack bad = throwError $ TypeMismatch "number" bad 
 	
